4b5f3d0c08ef49d04f58c978f5ad2b74261bbb2a
Emad <emadsiddiq@berkeley.edu>
2024-03-15 01:31:18 -0700

    Initial Commit

---
+++ Memorize2/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Memorize2
+//
+//  Created by Emad Siddiq on 3/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: false)
+            CardView(isFaceUp: true)
+        }
+        .foregroundColor(.orange)
+        .imageScale(.small)
+        .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack(content: {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+        })
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memorize2/Memorize2App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize2App.swift
+//  Memorize2
+//
+//  Created by Emad Siddiq on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Memorize2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
